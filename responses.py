64c100cbb5b58629314250bd165fd4a5b6e433c8
adeba4343-web <adeba4343@gmail.com>
2026-01-15 01:13:48 +0530

    Create responses.py

---
+++ responses.py
@@ -0,0 +1,13 @@
+responses = {
+    "hi": "Huh,What do you want? Why waste my time with dumb questions!",
+    "hello": "Hey there! Unfortunate to meet you.",
+    "hey": "you done?",
+    "how are you": "Obviously better than you in every field ...",
+    "what is your name": "WHY DO YOU CARE ? do your homework !",
+    "joke": "yea well look into the mirror you will find one.",
+    "bye": "gosh finally love it!!",
+    "i dont know what to say":"okay then go have some existential crisis within yourself..i aint available for this sh*t.",
+    "you so rude":"yeah deal with it",
+    "can you explain me wormholes?":"What? wormholes? well they are the holes created by worms dumboo isnt it in the name!?",
+    "i hate you":"awwwwwww that affected my soul! Oh wait i dont have one ",
+}
